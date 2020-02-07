9fe0309b89ae49fe8c5e447528ffe980043a2248
JuanSolis <juansolisgtsk@hotmail.com>
2020-02-06 21:13:29 -0600

    Updates

---
+++ precache-manifest.450b0b518c99918b45ff796ed9929b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b35c7bc4ba15a9bb532725cf1cb266",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "885c43be8b97a770cafd",
+    "url": "/react-monsters-rolodex/static/css/main.63cdda25.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/react-monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885c43be8b97a770cafd",
+    "url": "/react-monsters-rolodex/static/js/main.295cc711.chunk.js"
+  },
+  {
+    "revision": "2dca91bfe76b1dc69b8e",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.16565c76.js"
+  }
+]);
